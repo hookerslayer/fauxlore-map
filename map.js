19b396ba83b2b4a66d5bc60b9e16ea2e010d0676
hookerslayer <148587686+hookerslayer@users.noreply.github.com>
2025-02-17 17:50:21 +0300

    Update map.js

--- map.js
+++ map.js
@@ -103,7 +103,7 @@ fetch(url)
                 if (!isNaN(lat) && !isNaN(lng) && iconTypes[type]) {
                     // Создаем метку
                     var marker = L.marker([lat, lng], { icon: iconTypes[type] })
-                        .bindTooltip(name, { permanent: true, direction: 'right', offset: L.point(12, -15) })
+                        .bindTooltip(name, { permanent: true, direction: 'right', offset: L.point(10, -15) })
                         .bindPopup(`
                             <div class="popup-header">${name}</div>
                             <div class="popup-description">${description}</div>
