4697ea9056e0abd891663986fd867d33c88b37c2
hookerslayer <148587686+hookerslayer@users.noreply.github.com>
2025-02-17 21:14:55 +0300

    Update map.js

--- map.js
+++ map.js
@@ -103,11 +103,15 @@ fetch(url)
                 if (!isNaN(lat) && !isNaN(lng) && iconTypes[type]) {
                     // Создаем метку
                     var marker = L.marker([lat, lng], { icon: iconTypes[type] })
-                        .bindTooltip(name, { permanent: true, direction: 'bottom', offset: L.point(0, -3) })
                         .bindPopup(`
                             <div class="popup-header">${name}</div>
                             <div class="popup-description">${description}</div>
                         `);
+
+                    if (type !== 'Порт') {
+                        marker.bindTooltip(name, { permanent: true, direction: 'bottom', offset: L.point(0, -3) });
+                    }
+
                     // Добавляем метку в соответствующую группу
                     layers[type].addLayer(marker);
                 }
